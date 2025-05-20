29bb0a358844be3789eccf1a3d1b99cbc1b61b83
dahye <dahye9011@naver.com>
2025-05-20 14:51:08 +0900

    refactor: 클래스명 변경

--- src/main/java/com/example/demo/controller/OAuthController.java
+++ src/main/java/com/example/demo/controller/OAuthController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Auth API", description = "소셜 로그인 관련 API")
+@Tag(name = "OAuth API", description = "소셜 로그인 관련 API")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class OAuthController {
     private final GoogleOAuthService googleOAuthService;
 
     @Operation(summary = "사용자 등록", description = "구글 로그인으로 새로운 사용자를 등록합니다.")
