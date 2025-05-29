de2ab17209e33a3e68b2a11db417ca173a450913
dahye <dahye9011@naver.com>
2025-05-29 23:46:16 +0900

    refactor: DTO 구조 분리 반영

--- src/main/java/com/example/demo/mapper/PostMapper.java
+++ src/main/java/com/example/demo/mapper/PostMapper.java
@@ -1,6 +1,8 @@
 package com.example.demo.mapper;
 
-import com.example.demo.dto.PostDTO;
+import com.example.demo.dto.request.PostCreationDTO;
+import com.example.demo.dto.request.PostUpdateDTO;
+import com.example.demo.dto.response.PostResponseDTO;
 import com.example.demo.entity.Category;
 import com.example.demo.entity.Post;
 import com.example.demo.entity.UserEntity;
@@ -8,10 +10,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PostMapper {
-    public PostDTO toPostDto(Post post) {
+    public PostResponseDTO toPostResponseDto(Post post) {
         if (post == null) return null;
 
-        return PostDTO.builder()
+        return PostResponseDTO.builder()
                 .postId(post.getPostId())
                 .userId(post.getWriter().getUserId())
                 .content(post.getContent())
@@ -19,7 +21,7 @@ public class PostMapper {
                 .build();
     }
 
-    public Post toPostEntity(PostDTO dto, Category category, UserEntity writer) {
+    public Post toPostEntity(PostCreationDTO dto, Category category, UserEntity writer) {
         if (dto == null) return null;
 
         return Post.builder()
@@ -29,7 +31,7 @@ public class PostMapper {
                 .build();
     }
 
-    public void updatePostEntity(PostDTO dto, Post post, Category category) {
+    public void updatePostEntity(PostUpdateDTO dto, Post post, Category category) {
         if (dto.getContent() != null) {
             post.updateContent(dto.getContent());
         }
