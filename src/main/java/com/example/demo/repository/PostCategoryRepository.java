47235bf9e38090f45cdfb5ebb4a714449663df7e
dahye <dahye9011@naver.com>
2025-05-29 23:48:03 +0900

    refactor: Post 및 PostCategory 리포지토리 수정

--- src/main/java/com/example/demo/repository/PostCategoryRepository.java
+++ src/main/java/com/example/demo/repository/PostCategoryRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.PostCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostCategoryRepository extends JpaRepository<PostCategory, Long> {
+
 }
 
 
