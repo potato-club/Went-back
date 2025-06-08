482a59a7466181bbbbd09b9cfd8f3f4c57fa3e2a
dahye <dahye9011@naver.com>
2025-06-08 21:54:11 +0900

    feat: 마이페이지 응답 DTO 및 매퍼 클래스 추가

--- src/main/java/com/example/demo/mapper/CategoryMapper.java
+++ src/main/java/com/example/demo/mapper/CategoryMapper.java
@@ -1,4 +1,15 @@
 package com.example.demo.mapper;
 
+import com.example.demo.dto.response.CategoryResponseDTO;
+import com.example.demo.entity.Category;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CategoryMapper {
+    public CategoryResponseDTO toCategoryResponseDTO(Category category) {
+        return CategoryResponseDTO.builder()
+                .id(category.getCategoryId())
+                .name(category.getName())
+                .build();
+    }
 }
