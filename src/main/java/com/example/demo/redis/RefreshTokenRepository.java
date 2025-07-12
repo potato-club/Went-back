9f0cdb52e64a25a5b015cb1d10b4e741e43f6e69
SeungjunS0ng <john091122@naver.com>
2025-07-12 21:50:37 +0900

    chore: remove redis config

--- src/main/java/com/example/demo/redis/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-
-package com.example.demo.redis;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
