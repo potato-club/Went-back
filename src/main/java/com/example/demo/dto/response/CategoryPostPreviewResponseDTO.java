84d753a9295cb58fe2f6ac0c9c7fb6807d50cda5
dahye <dahye9011@naver.com>
2025-07-19 01:50:10 +0900

    feat: 메인페이지 및 사용자 관련 DTO 추가 및 수정

--- src/main/java/com/example/demo/dto/response/CategoryPostPreviewResponseDTO.java
+++ src/main/java/com/example/demo/dto/response/CategoryPostPreviewResponseDTO.java
@@ -1,4 +1,14 @@
 package com.example.demo.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
 public class CategoryPostPreviewResponseDTO {
+    private Long categoryId;
+    private String categoryName;
+    private List<PostPreviewResponseDTO> posts;
 }
--- src/main/java/com/example/demo/dto/response/MainContentResponseDTO.java
+++ src/main/java/com/example/demo/dto/response/MainContentResponseDTO.java
@@ -1,4 +1,13 @@
 package com.example.demo.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
 public class MainContentResponseDTO {
+    private UserHomeResponseDTO user;
+    private List<CategoryPostPreviewResponseDTO> posts;
 }
