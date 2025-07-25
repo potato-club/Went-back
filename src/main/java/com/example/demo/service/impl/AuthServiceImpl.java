2066fb7491eca2c773d85072ca25cbeb92ac00b9
SeungjunS0ng <john091122@naver.com>
2025-07-25 14:55:13 +0900

    feat: 리프레시 토큰 Redis 저장 기능 추가
    - AuthServiceImpl.reissueToken()에서 리프레시 토큰을 Redis에 저장하도록 구현
    - RefreshTokenRepository를 활용해 email을 키로, 토큰을 값으로 저장

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.demo.error.InvalidTokenException;
 import com.example.demo.error.UnAuthorizedException;
 import com.example.demo.jwt.JwtProvider;
 import com.example.demo.jwt.JwtToken;
+import com.example.demo.redis.RefreshToken;
+import com.example.demo.redis.RefreshTokenRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.AuthService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
     private final JwtProvider jwtProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
 
     // 토큰 재발급
     public void reissueToken(HttpServletResponse response, String refreshToken) {
@@ -28,6 +32,14 @@ public class AuthServiceImpl implements AuthService {
         }
 
         JwtToken jwtToken = jwtProvider.reissueToken(refreshToken);
+
+        String email = jwtProvider.getUsernameFromToken(jwtToken.getRefreshToken());
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .email(email)
+                        .token(jwtToken.getRefreshToken())
+                        .build()
+        );
 
         jwtProvider.setHeaderAccessToken(response, jwtToken.getAccessToken());
         jwtProvider.setHeaderRefreshToken(response, jwtToken.getRefreshToken());
