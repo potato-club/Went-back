9f92b7418bd711ffbb7862e52ba654ad4ceaa719
SeungjunS0ng <john091122@naver.com>
2025-07-25 14:42:58 +0900

    test redis

---
+++ src/main/java/com/example/demo/redis/RefreshToken.java
@@ -0,0 +1,21 @@
+package com.example.demo.redis;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24) //24시간 (초 단위)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+
+    @Id
+    private String email;  // key 역할 (식별자)
+
+
+    @Setter
+    private String token;  // 저장할 refresh 토큰
+
+}
