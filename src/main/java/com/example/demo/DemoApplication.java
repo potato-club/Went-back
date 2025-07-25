d3db25b0eaf012a3c22642afd13910453169274e
SeungjunS0ng <john091122@naver.com>
2025-07-25 15:12:55 +0900

    feat(config): @EnableRedisRepositories 애노테이션 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRedisRepositories
 public class DemoApplication {
 
 	public static void main(String[] args) {
